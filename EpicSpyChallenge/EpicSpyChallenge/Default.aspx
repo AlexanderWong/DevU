c5fd0f6640c5dc74a7eff7007926c40580e4d1b3
Alex Wong <code.alexanderwong@gmail.com>
2018-02-16 12:27:34 -0800

    Spy Challenge Commit

---
+++ EpicSpyChallenge/EpicSpyChallenge/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EpicSpyChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="spyImage" runat="server" Height="190px" ImageUrl="~/epic-spies-logo.jpg" />
+            <br />
+            <h2>Spy New Assignment Form</h2>
+            <br />
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="codenameTextBox" runat="server" OnTextChanged="codenameTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="assignmentnameTextBox" runat="server" OnTextChanged="assignmentnameTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment<asp:Calendar ID="endPreviousCalendar" runat="server" OnSelectionChanged="endPreviousCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            Start Date of New Assignment<asp:Calendar ID="startNewCalendar" runat="server" OnSelectionChanged="startNewCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            Projected End Date of New Assignment<asp:Calendar ID="projectedEndOfNewCalendar" runat="server" OnSelectionChanged="projectedEndOfNewCalendar_SelectionChanged"></asp:Calendar>
+            <br />
+            <asp:Button ID="assignButton" runat="server" Text="Assign Spy" OnClick="assignButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EpicSpyChallenge/EpicSpyChallenge/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EpicSpyChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                endPreviousCalendar.SelectedDate = DateTime.Now.Date;
+                startNewCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                projectedEndOfNewCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+        }
+
+        protected void codenameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignmentnameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void endPreviousCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void startNewCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void projectedEndOfNewCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+
+            double daysOff = double.Parse(startNewCalendar.SelectedDate.Subtract(endPreviousCalendar.SelectedDate).TotalDays.ToString());
+            double workDays = double.Parse(projectedEndOfNewCalendar.SelectedDate.Subtract(startNewCalendar.SelectedDate).TotalDays.ToString());
+            if (daysOff < 14)
+            {
+                startNewCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                resultLabel.Text = "Error: Please allow the agent to rest for 14 days.";
+            }
+
+            if (14 <= daysOff && workDays <= 21 )
+            {
+              
+                double totalMoney = workDays * 500;
+                resultLabel.Text = "The assignment of Agent " + codenameTextBox.Text + " to " + assignmentnameTextBox.Text + " is authorized with a budget of $" + totalMoney + ".";
+            }
+
+             if (14 <= daysOff && 21 < workDays)
+            {
+                double totalMoney = ((workDays * 500) + 1000);
+                resultLabel.Text = "The assignment of Agent " + codenameTextBox.Text + " to " + assignmentnameTextBox.Text + " is authorized with a budget of $" + totalMoney + ".";
+            }
+        }
+    }
+}
