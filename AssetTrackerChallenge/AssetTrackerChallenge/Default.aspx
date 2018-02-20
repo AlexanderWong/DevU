ee0aa4f5da38cad80c8694efd453c39f8f4a4ddd
Alex Wong <code.alexanderwong@gmail.com>
2018-02-20 11:28:45 -0800

    Asset Tracker Challenge

---
+++ AssetTrackerChallenge/AssetTrackerChallenge/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AssetTrackerChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Calibri;
+        }
+        .auto-style2 {
+            color: #333333;
+            background-color: #FFFFFF;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="150px" ImageUrl="~/epic-spies-logo.jpg" />
+            <h2 class="auto-style1"><em><span class="auto-style2">Asset Performance Tracker</span></em></h2>
+            Asset Name:&nbsp;
+            <asp:TextBox ID="assetsTextBox" runat="server" OnTextChanged="assetTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:&nbsp; <asp:TextBox ID="riggedTextBox" runat="server" OnTextChanged="riggedTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:&nbsp;
+            <asp:TextBox ID="subterfugeTextBox" runat="server" OnTextChanged="subterfugeTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AssetTrackerChallenge/AssetTrackerChallenge/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AssetTrackerChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                double[] rigged = new double[0];
+                ViewState.Add("Rigged", rigged);
+
+                double[] subterfuge = new double[0];
+                ViewState.Add("Subterfuge", subterfuge);
+
+                string[] assets = new string[0];
+                ViewState.Add("Assets", assets);
+            }
+        }
+
+        protected void assetTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void riggedTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void subterfugeTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            double[] rigged = (double[])ViewState["Rigged"];
+            Array.Resize(ref rigged, rigged.Length + 1);
+            int newRiggedItem = rigged.GetUpperBound(0);
+            rigged[newRiggedItem] = double.Parse(riggedTextBox.Text);
+            ViewState["Rigged"] = rigged;
+
+            double[] subterfuge = (double[])ViewState["Subterfuge"];
+            Array.Resize(ref subterfuge, subterfuge.Length + 1);
+            int newSubterfugeItem = subterfuge.GetUpperBound(0);
+            subterfuge[newSubterfugeItem] = double.Parse(subterfugeTextBox.Text);
+            ViewState["Subterfuge"] = subterfuge;
+
+            string[] assets = (string[])ViewState["Assets"];
+            Array.Resize(ref assets, assets.Length + 1);
+            int newAssetsItem = assets.GetUpperBound(0);
+            assets[newAssetsItem] = assetsTextBox.Text;
+            ViewState["Assets"] = assets;
+
+            resultLabel.Text = String.Format("Total number of elections rigged: {0}<br />Average acts of subterfuge per asset: {1:N2}<br />(Last asset added: {2})", rigged.Sum(), subterfuge.Average(), assets[newAssetsItem]);
+
+            assetsTextBox.Text = "";
+            riggedTextBox.Text = "";
+            subterfugeTextBox.Text = "";
+
+
+        }
+    }
+}
