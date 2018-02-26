a0a3b43446037ab9a7d519177a17ce22e3d6e967
Alex Wong <code.alexanderwong@gmail.com>
2018-02-26 15:23:57 -0800

    Mega Casino Challenge

---
+++ MegaCasinoChallenge/MegaCasinoChallenge/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MegaCasinoChallenge.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Georgia, "Times New Roman", Times, serif;
+        }
+        .auto-style2 {
+            background-color: #FFCC00;
+        }
+        .auto-style3 {
+            text-decoration: underline;
+        }
+        .auto-style6 {
+            font-family: Calibri;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2 class="auto-style1"><em><span class="auto-style2">Welcome to the Casino! Are you ready to win big?</span></em></h2>
+            <br />
+            <asp:Image ID="Image1" runat="server" Height="165px" Width="157px" />
+            <asp:Image ID="Image2" runat="server" Height="165px" Width="157px" />
+            <asp:Image ID="Image3" runat="server" Height="165px" Width="157px" />
+            <br />
+            <br />
+            Your Bet:&nbsp; <asp:TextBox ID="betTextBox" runat="server" OnTextChanged="betTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="pullLeverButton" runat="server" OnClick="pullLeverButton_Click" Text="Pull Lever!" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="moneyLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            1 Cherry - x2 Your Bet<br />
+            2 Cherry - x3 Your Bet<br />
+            3 Cherry - x4 Your Bet<br />
+            3 7&#39;s - JACKPOT - x100 Your Bet<br />
+            HOWEVER... if there is even <em>ONE</em> <strong>BAR</strong> you win <span class="auto-style3">NOTHING</span>!</div>
+    </form>
+</body>
+</html>
---
+++ MegaCasinoChallenge/MegaCasinoChallenge/Default.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaCasinoChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        Random random = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack) 
+            {
+                string[] lever = new string[] { imageGenerator(), imageGenerator(), imageGenerator() };
+                showImages(lever);
+                ViewState.Add("PlayersMoney", 100);
+                displayPlayersMoney();
+            }
+    
+        }    
+
+        protected void betTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        protected void pullLeverButton_Click(object sender, EventArgs e)
+        {          
+            int bet = 0;
+            if (!int.TryParse(betTextBox.Text, out bet)) return;
+            int winnings = pullLever(bet);
+            changePlayerMoney(bet, winnings);
+            winningPull(bet, winnings);
+            displayPlayersMoney();
+        }
+
+        private void changePlayerMoney(int bet, int winnings)
+        {
+            int playersMoney = int.Parse(ViewState["PlayersMoney"].ToString());
+            playersMoney -= bet;
+            playersMoney += winnings;
+            ViewState["PlayersMoney"] = playersMoney;
+        }
+
+       private void winningPull(int bet, int winnings)
+        {
+            if (winnings > 0)
+                resultLabel.Text = String.Format("You bet {0:C} and won {1:C}", bet, winnings);
+            else
+                resultLabel.Text = String.Format("Sorry you lost {0:C}, try again!", bet);
+        }
+
+        private int pullLever(int bet)
+        {
+            string[] lever = new string[] { imageGenerator(), imageGenerator(), imageGenerator()};
+            showImages(lever);
+
+            int multiplier = evaluateLever(lever);
+            return bet * multiplier;
+        }
+
+        private int evaluateLever(string[] lever)
+        {
+            if (barResult(lever)) return 0;
+            if (sevenResult(lever)) return 100;   
+            int multiplier = 0;
+            if (cherry(lever, out multiplier)) return multiplier;
+            return 0;
+        }
+
+        private bool cherry(string[] lever, out int multiplier)
+        {
+            multiplier = cherryMultiplier(lever);
+            if (multiplier > 0) return true;
+            return false;
+                     
+        }
+
+        private void showImages(string[] lever)
+        {
+            Image1.ImageUrl = lever[0] + ".png";
+            Image2.ImageUrl = lever[1] + ".png";
+            Image3.ImageUrl = lever[2] + ".png";
+        }
+
+        private string imageGenerator()
+        {
+            string[] images = new string[] { "Strawberry", "Bar", "Lemon", "Bell", "Clover", "Cherry", "Diamond", "Orange", "Seven", "HorseShoe", "Plum", "Watermelon" };
+            return images[random.Next(11)];
+        }
+
+        private void displayPlayersMoney()
+        {
+            moneyLabel.Text = String.Format("Your Money: {0:C}", ViewState["PlayersMoney"]);
+        }
+
+
+        /*================================================= SPIN RESULTS ====================================================*/
+        private bool barResult(string[] lever)
+        {
+            if (lever[0] == "Bar" || lever[1] == "Bar" || lever[2] == "Bar") return true;
+            else return false;
+        }
+
+        private bool sevenResult(string[] lever)
+        {
+            if (lever[0] == "Seven" && lever[1] == "Seven" && lever[2] == "Seven") return true;
+            else return false;
+        }
+
+        /*============================================= END SPIN RESULTS ====================================================*/
+
+        /*================================================ MULTIPLIER =======================================================*/
+        private int cherryMultiplier(string[] lever)
+        {
+            int cherryCount = totalCherryCount(lever);
+            if (cherryCount == 1) return 2;
+            if (cherryCount == 2) return 3;
+            if (cherryCount == 3) return 4;
+            return 0;
+        }
+
+        private int totalCherryCount(string[] lever)
+        {
+            int cherryCount = 0;
+            if (lever[0] == "Cherry") cherryCount++;
+            if (lever[1] == "Cherry") cherryCount++;
+            if (lever[2] == "Cherry") cherryCount++;
+            return cherryCount;
+        }
+      
+
+        /*============================================== END MULTIPLIER ===================================================*/
+
+    }
+}
