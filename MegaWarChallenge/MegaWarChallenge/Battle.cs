645b0886aee1fb1568689bf9695ff82b81f14a99
Alex Wong <code.alexanderwong@gmail.com>
2018-03-19 09:23:14 -0700

    Mega War Challenge

---
+++ MegaWarChallenge/MegaWarChallenge/Battle.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace MegaWarChallenge
+{
+    public class Battle
+    {
+        private List<Card> _loot;
+        private StringBuilder _sb;
+
+        public Battle ()
+        {
+            _loot = new List<Card>();
+            _sb = new StringBuilder();
+        }
+
+        public string PerformBattle(Player player1, Player player2)
+        {
+            Card player1Card = getCard(player1);
+            Card player2Card = getCard(player2);
+
+            compareCardValues(player1, player2, player1Card, player2Card);
+            return _sb.ToString();
+        }
+        
+        private Card getCard(Player player)
+        {
+            Card card = player.Cards.ElementAt(0);
+            player.Cards.Remove(card);
+            _loot.Add(card);
+            return card;
+        }
+
+        private void compareCardValues(Player player1, Player player2, Card card1, Card card2)
+        {
+            displayWarCards(card1, card2);
+            if (card1.CardValue() == card2.CardValue())
+                war(player1,player2);
+            if (card1.CardValue() > card2.CardValue())
+                awardWinner(player1);
+            else
+                awardWinner(player2);
+            
+        }
+
+        private void awardWinner(Player player)
+        {
+            if (_loot.Count == 0) return;
+            displayLoot();
+            player.Cards.AddRange(_loot);
+            _loot.Clear();
+            _sb.Append("<br/><strong>");
+            _sb.Append(player.Name);
+            _sb.Append(" wins!</strong><br/>");
+        }
+
+        private void war(Player player1, Player player2)
+        {
+            _sb.Append("</br>============== I DECLARE WAR =================<br/>");
+            getCard(player1);
+            Card warCard1 = getCard(player1);
+            getCard(player1);
+            
+
+
+            getCard(player2);
+            Card warCard2 = getCard(player2);
+            getCard(player2);
+            
+
+            compareCardValues(player1, player2, warCard1, warCard2);
+        }
+
+        private void displayWarCards(Card card1, Card card2)
+        {
+            _sb.Append("<br/>Cards to fight: ");
+            _sb.Append(card1.Kind);
+            _sb.Append(" of ");
+            _sb.Append(card1.Suit);
+            _sb.Append(" vs. ");
+            _sb.Append(card2.Kind);
+            _sb.Append(" of ");
+            _sb.Append(card2.Suit);
+
+        }
+
+        private void displayLoot()
+        {
+            _sb.Append("<br/> Loot");
+            foreach (var card in _loot)
+            {
+                _sb.Append("<br/>&nbsp;&nbsp;&nbsp;&nbsp;");
+                _sb.Append(card.Kind);
+                _sb.Append(" of ");
+                _sb.Append(card.Suit);
+            }         
+        }
+
+    }
+}
---
+++ MegaWarChallenge/MegaWarChallenge/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaWarChallenge
+{
+    public class Game
+    {
+        private Player _player1;
+        private Player _player2;
+        
+
+        public Game(string player1Name, string player2Name)
+        {
+            _player1 = new Player() { Name = player1Name };
+            _player2 = new Player() { Name = player2Name };
+            
+        }
+
+        public string Play()
+        {
+
+            Deck deck = new Deck();
+            int round = 0;
+            string result = "<h2>Distributing weapons to each faction...</h2>";
+                result += deck.Deal(_player1, _player2);
+                result += "<h2>Let the war begin...</h2>";
+
+            while (_player1.Cards.Count != 0 && _player2.Cards.Count != 0)
+            {
+                Battle battle = new Battle();
+                result += battle.PerformBattle(_player1, _player2);
+
+                round++;
+                if (round > 20)
+                    break;
+            }
+            result += determineWinner();
+            return result;
+            
+        }
+       
+        private string determineWinner()
+        {
+            string result = "";
+            if (_player1.Cards.Count > _player2.Cards.Count)
+                result += "<br/></strong>Bill wins the war!";
+            else
+                result += "<br/</strong>Ted wins the war!";
+            result += "<br/>Bill's Card Count:" + _player1.Cards.Count + " Ted's Card Count:" + _player2.Cards.Count;
+            return result;
+        }       
+    }
+}
+
+    
